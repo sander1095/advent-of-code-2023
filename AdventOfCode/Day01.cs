231ed513ed6f5b378ea9fe8119dbc3bcb4127fbb
Sander ten Brinke <s.tenbrinke2@gmail.com>
2023-12-01 19:36:21 +0100

    First solution for day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,26 @@
-﻿namespace AdventOfCode;
+﻿using System.Linq;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly ICollection<string> _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{_input
+    .Select(x =>
+    {
+        var firstCharNumber = x.First(x => ((short)x) is > 47 and < 58);
+        var lastCharNumber = x.Last(x => ((short)x) is > 47 and < 58);
+
+        var combinedString = $"{firstCharNumber}{lastCharNumber}";
+        var total = int.Parse(combinedString);
+        return total;
+    }).Sum()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
